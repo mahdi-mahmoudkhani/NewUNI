72a4ec52959a995d81bf3bc76d758806a4fa383e
Mahdi <125703793+mahdi-mahmoudkhani@users.noreply.github.com>
2024-08-07 12:18:41 +0330

    feat(Prof): Added Prof struct to Entity files with its init.

---
+++ Entities/Prof.swift
@@ -0,0 +1,22 @@
+//
+//  Prof.swift
+//  NewUNI
+//
+//  Created by Mahdi on 5/17/1403 AP.
+//
+
+import Foundation
+
+struct Prof {
+    let firstName: String
+    let lastName: String
+    let profID: Int
+    var supportStudents: [Student]?
+    
+    init(firstName: String, lastName: String, profID: Int, supportStudents: [Student]? = nil) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.profID = profID
+        self.supportStudents = supportStudents
+    }
+}
