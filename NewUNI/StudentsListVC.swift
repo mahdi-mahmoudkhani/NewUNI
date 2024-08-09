034cabc007e35ba9ff482079985eb284a19f93b0
Mahdi <125703793+mahdi-mahmoudkhani@users.noreply.github.com>
2024-08-09 20:04:59 +0330

    feat: Add `StudentsList` storyboard and conncect it to `StudentsListVC`
        - Modified the Info.plist to point to the `StudentsList` storyboard

---
+++ NewUNI/StudentsListVC.swift
@@ -0,0 +1,29 @@
+//
+//  StudentsListVC.swift
+//  NewUNI
+//
+//  Created by Mahdi on 5/19/1403 AP.
+//
+
+import UIKit
+
+class StudentsListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
