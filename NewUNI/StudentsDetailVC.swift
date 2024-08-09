32d9aa14e524936ff9acf0e417912ede85a0f9fb
Mahdi <125703793+mahdi-mahmoudkhani@users.noreply.github.com>
2024-08-10 02:12:35 +0330

    feat: Add UI elements for course input in StudentsDetailVC
    - Added `UITextField` outlet `newCourseField` and `UIButton` outlet `addCourseButton` in `StudentsDetailVC`
    - Configured UI elements in storyboard with constraints

--- NewUNI/StudentsDetailVC.swift
+++ NewUNI/StudentsDetailVC.swift
@@ -14,6 +14,8 @@ class StudentsDetailVC: UIViewController {
     @IBOutlet weak var studentIdField: UILabel!
     @IBOutlet weak var profNameField: UILabel!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var newCourseField: UITextField!
+    @IBOutlet weak var addCourseButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
