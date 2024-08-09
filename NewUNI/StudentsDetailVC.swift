12a6d327c8c5d8915ed95c16f122dc8f623458d5
Mahdi <125703793+mahdi-mahmoudkhani@users.noreply.github.com>
2024-08-10 02:17:24 +0330

    feat: Enable addCourseButton based on newCourseField input
    - Implemented `newCourseField(_:)` action to enable `addCourseButton` when `newCourseField` is not empty

--- NewUNI/StudentsDetailVC.swift
+++ NewUNI/StudentsDetailVC.swift
@@ -25,17 +25,10 @@ class StudentsDetailVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func newCourseField(_ sender: Any) {
+        
+        addCourseButton.isEnabled = newCourseField.text != ""
     }
-    */
-
 }
 
 extension StudentsDetailVC: UITabBarDelegate, UITableViewDataSource {
