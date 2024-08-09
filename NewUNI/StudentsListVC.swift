88b7039a9140b300a89a7241abe2c3868d66d6ba
Mahdi <125703793+mahdi-mahmoudkhani@users.noreply.github.com>
2024-08-09 21:15:08 +0330

    feat: Add button enable logic to text field editing
    - Added @IBAction `StudentDataCompleted` to enable `addNewStudentBottom` button based on non-empty text fields.
    - Connected `StudentDataCompleted` action to "Editing Changed" event for both `newStudentFirstNameField` and `newStudentLastNameField`.

--- NewUNI/StudentsListVC.swift
+++ NewUNI/StudentsListVC.swift
@@ -24,17 +24,11 @@ class StudentsListVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func StudentDataCompleted(_ sender: Any) {
+        
+        addNewStudentBottom.isEnabled = newStudentLastNameField.text != "" && newStudentFirstNameField.text != ""
     }
-    */
-
+    
 }
 
 extension StudentsListVC: UITableViewDelegate, UITableViewDataSource {
